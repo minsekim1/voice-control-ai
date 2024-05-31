7d33e98e50608f71ff115d03c755cd8f3ba40639
minsekim1 <tkarnrwl7862@naver.com>
2024-06-01 01:35:32 +0900

    exit -> Ctrl+C or cmd+C

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ voice controller for computer AI
 ## env execute
 
 `source env/bin/activate`
+
+## program execute
+
+`python3 app.py`
--- app.py
+++ app.py
@@ -11,21 +11,31 @@ class TransparentWindow(QtWidgets.QWidget):
         self.setGeometry(100, 100, 400, 200)  # 창 위치와 크기 설정
 
         layout = QtWidgets.QVBoxLayout()
-        label = QtWidgets.QLabel("on Voice", self)
-        label.setFont(QtGui.QFont('Arial', 30, QtGui.QFont.Bold))
-        label.setStyleSheet("color: white; background: transparent;")  # 텍스트 색상을 흰색으로 설정
+        self.label = QtWidgets.QLabel("on Voice", self)
+        self.label.setFont(QtGui.QFont('Arial', 30, QtGui.QFont.Bold))
+        self.label.setStyleSheet("color: white; background: transparent;")
         
         # 텍스트에 그림자 효과 추가
         shadow = QtWidgets.QGraphicsDropShadowEffect()
         shadow.setBlurRadius(15)
         shadow.setColor(QtGui.QColor('black'))
         shadow.setOffset(1, 1)
-        label.setGraphicsEffect(shadow)
+        self.label.setGraphicsEffect(shadow)
 
-        layout.addWidget(label)
+        layout.addWidget(self.label)
 
         self.setLayout(layout)
         self.show()
+
+    def keyPressEvent(self, event):
+        if event.key() == QtCore.Qt.Key_Space:
+            self.label.setStyleSheet("color: red; background: transparent;")
+        elif (event.modifiers() & (QtCore.Qt.ControlModifier | QtCore.Qt.MetaModifier)) and event.key() == QtCore.Qt.Key_C:
+            self.close()
+
+    def keyReleaseEvent(self, event):
+        if event.key() == QtCore.Qt.Key_Space:
+            self.label.setStyleSheet("color: white; background: transparent;")
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication([])
