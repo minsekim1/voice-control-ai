15c79f4f9b1864bed8c539e5c0e38b245d7b3b85
minsekim1 <tkarnrwl7862@naver.com>
2024-06-14 13:56:54 +0900

    음성인식 개선

--- module/catch_vocie.py
+++ module/catch_vocie.py
@@ -19,9 +19,23 @@ def execute_command(command):
     elif "여보세요" in command:
         print("BOT: 안녕?")
         return True
+    elif "혼내줘" in command:
+        print("BOT: 김정예 혼날래!")
+        return True
+    elif "컴퓨터 재시작" in command:
+        print("BOT: 컴퓨터를 재시작합니다.")
+        os.system("shutdown /r /t 1")
+        return True
+    elif "컴퓨터 종료" in command:
+        print("BOT: 컴퓨터를 종료합니다.")
+        os.system("shutdown /s /t 1")
+        return True
+    elif "컴퓨터 절전" in command:
+        print("BOT: 컴퓨터를 절전 모드로 전환합니다.")
+        os.system("rundll32.exe powrprof.dll,SetSuspendState 0,1,0")
     elif "종료" in command:
         print("BOT: 프로그램을 종료합니다.")
         sys.exit()  # 프로그램 종료
         return True
     else :
-    		return False
+        return False
