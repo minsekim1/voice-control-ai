f14f105b0be17726184fef8f8c6d562ec7550bd2
minsekim1 <tkarnrwl7862@naver.com>
2024-06-14 11:08:12 +0900

    메모장 실행성공

--- module/recognizeSpeech.py
+++ module/recognizeSpeech.py
@@ -1,3 +1,4 @@
+import os
 import sys
 import speech_recognition as sr
 
@@ -14,11 +15,15 @@ def recognizeSpeech(recognizer, microphone):
         
         # 특정 명령어가 텍스트에 포함되어 있으면 프로그램 종료
         exitProgram(text)
+
+				# 메모장 키기
+        exit_commands = ["메모장"]
+        for command in exit_commands:
+            if command in text:
+                print("메모장 프로그램을 실행합니다.")
+                os.system("notepad.exe")
             
-    # except sr.UnknownValueError:
-    #     print("Google Speech Recognition could not understand audio")
-    except sr.RequestError as e:
-        # print("Could not request results from Google Speech Recognition service; {0}".format(e))
+    except Exception as e:
         print("No message")
         
 # STT 서비스 시작하기
