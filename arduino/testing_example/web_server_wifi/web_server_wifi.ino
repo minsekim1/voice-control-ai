5548b9d3d2463291518c2fc9d2cf55f8c82dde72
minsekim1 <tkarnrwl7862@naver.com>
2024-09-22 10:17:48 +0900

    와이파이 재연결기능 추가:성공

--- arduino/testing_example/web_server_wifi/web_server_wifi.ino
+++ arduino/testing_example/web_server_wifi/web_server_wifi.ino
@@ -19,6 +19,8 @@ void setup() {
     Serial.begin(115200);
     Serial.println();
     Serial.println("Configuring access point...");
+    // 혼합 모드로 설정: 스테이션 + 액세스 포인트 모드
+    WiFi.mode(WIFI_AP_STA);
     WiFi.softAP(ssid, password);
     IPAddress myIP = WiFi.softAPIP();
     Serial.print("AP IP address: ");
@@ -107,12 +109,15 @@ void loop() {
 
 // 입력받은 SSID가 주변에 있는지 확인하는 함수
 bool isSSIDAvailable(String ssid) {
-    WiFi.disconnect();  // 네트워크 상태를 초기화
+    WiFi.disconnect(true);  // 이전 연결 및 캐싱된 정보 모두 삭제
+    WiFi.scanDelete();  // 이전 스캔 결과 삭제
     delay(1000);        // 약간의 지연 시간 후에 스캔 시작
 
     Serial.println("Scanning for available networks...");
     int numNetworks = WiFi.scanNetworks();  // Wi-Fi 네트워크 스캔
+    
     for (int i = 0; i < numNetworks; i++) {
+        Serial.println("Scanning for available networks..." + ssid);
         if (WiFi.SSID(i) == ssid) {  // 입력된 SSID가 존재하는지 확인
             Serial.print("Found SSID: ");
             Serial.println(ssid);
@@ -126,7 +131,7 @@ bool isSSIDAvailable(String ssid) {
 // 클라이언트 모드로 전환하여 Wi-Fi 네트워크에 연결하는 함수
 void connectToWiFi(WiFiClient client, String ssid, String password) {
     // 연결을 시도하기 전에 WiFi 상태를 초기화
-    WiFi.disconnect();  // 이전 연결 종료
+    WiFi.disconnect(true);  // 이전 연결 및 캐싱된 정보 모두 삭제
     delay(1000);        // 약간의 지연을 줘서 네트워크 모듈이 리셋될 시간을 확보
 
     Serial.println("Attempting to connect to Wi-Fi...");
