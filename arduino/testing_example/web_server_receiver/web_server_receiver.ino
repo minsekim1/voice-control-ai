c62e00147ff059f5baf0059ab4899b5ce2dce5d0
minsekim1 <tkarnrwl7862@naver.com>
2024-09-22 16:44:34 +0900

    web_server_receiver.ino 수정중

--- arduino/testing_example/web_server_receiver/web_server_receiver.ino
+++ arduino/testing_example/web_server_receiver/web_server_receiver.ino
@@ -16,14 +16,17 @@ void setup() {
 
     Serial.begin(115200);
     Serial.println();
+    Serial.print("[System] ");
     Serial.println("Configuring access point...");
     // 혼합 모드로 설정: 스테이션 + 액세스 포인트 모드
     WiFi.mode(WIFI_AP_STA);
     WiFi.softAP(ssid, password);
     IPAddress myIP = WiFi.softAPIP();
     Serial.print("AP IP address: ");
+    Serial.print("[System] ");
     Serial.println(myIP);
     server.begin();
+    Serial.print("[System] ");
     Serial.println("Server started");
 
 #pragma region 저장된 와이파이 ssid/pw 가져오기
@@ -35,6 +38,7 @@ void setup() {
 
     if (saved_ssid != "" && saved_password != "") {
         // 저장된 Wi-Fi 정보가 있다면 스테이션 모드로 연결 시도
+        Serial.print("[System] ");
         Serial.println("Attempting to connect to saved Wi-Fi...");
         WiFiClient client = server.available();
         connectToWiFi(client, saved_ssid, saved_password);
@@ -45,7 +49,8 @@ void setup() {
 void loop() {
     WiFiClient client = server.available();  // 클라이언트 접속 감지
     if (client) {                            // 만약 클라이언트가 감지되면
-        Serial.println(" >> New Client.");       // 클라이언트 연결 메시지 출력
+        Serial.print(" >> ");
+        Serial.println("New Client.");       // 클라이언트 연결 메시지 출력
         char buffer[300];                    // 데이터를 저장할 버퍼 선언 (300바이트 크기)
         int length;                          // 읽어들인 데이터의 길이를 저장할 변수
         bool requestHandled = false;         // 요청이 처리되었는지 여부를 체크하는 플래그
@@ -56,6 +61,7 @@ void loop() {
                 length = client.readBytesUntil('\n', buffer, sizeof(buffer) - 1);
                 buffer[length] = 0;               // 읽어들인 데이터를 문자열로 처리하기 위해 마지막에 널 문자 추가
                 String request = String(buffer);  // 버퍼 내용을 String 객체로 변환
+                Serial.print(" >> ");
                 Serial.println(request);          // 클라이언트 요청을 시리얼 모니터에 출력
 
                 // HTTP 요청의 끝은 빈 줄로 표시되므로, 요청이 끝났으면 응답을 보냄
@@ -68,6 +74,7 @@ void loop() {
 #pragma region 응답 처리
                     if (request.indexOf("GET /on") >= 0) {
                         digitalWrite(pin8, LOW);  // LED 켜기
+                        Serial.print(" >> ");
                         Serial.println("LED ON");
 
                         // HTTP 응답의 내용
@@ -75,19 +82,23 @@ void loop() {
                         client.print("Click <a href=\"/\">here</a> to commands list<br/>");
                     } else if (request.indexOf("GET /off") >= 0) {
                         digitalWrite(pin8, HIGH);  // LED 끄기
+                        Serial.print(" >> ");
                         Serial.println("LED OFF");
 
                         // HTTP 응답의 내용
                         client.print("<p>LED off complete</p>");
                         client.print("Click <a href=\"/\">here</a> to commands list<br/>");
                     } else if (request.indexOf("GET /wifi") >= 0) {
+                        Serial.print(" >> ");
                         Serial.println("Wifi connect");
 
                         // 1. SSID와 Password 추출
                         String ssid = request.substring(request.indexOf("ssid=") + 5, request.indexOf("&"));
                         String password = request.substring(request.indexOf("password=") + 9, request.indexOf(" ", request.indexOf("password=")));
+                        Serial.print(" >> ");
                         Serial.print("SSID: ");
                         Serial.println(ssid);
+                        Serial.print(" >> ");
                         Serial.print("Password: ");
                         Serial.println(password);
 
@@ -103,14 +114,17 @@ void loop() {
                         // HTTP 응답의 내용
                         client.print("Click <a href=\"/\">here</a> to commands list<br/>");
                     }else if (request.indexOf("GET /pin") >= 0) {
+                        Serial.print(" >> ");
                         Serial.println("LED Pin control");
 
                         // 1. pin 번호와 value 추출
                         int pinNumber = request.substring(request.indexOf("pin=") + 4, request.indexOf("&")).toInt();
                         String pinValue = request.substring(request.indexOf("value=") + 6, request.indexOf(" ", request.indexOf("value=")));
-    
+                        
+                        Serial.print(" >> ");
                         Serial.print("pinNumber: ");
                         Serial.println(pinNumber);
+                        Serial.print(" >> ");
                         Serial.print("pinValue: ");
                         Serial.println(pinValue);
 
@@ -126,11 +140,14 @@ void loop() {
                         // 3. LED 컨트롤
                         if (pinValue == "on") {
                             digitalWrite(pinNumber, LOW);  // LED 켜기
+                            Serial.print(" >> ");
                             Serial.println("LED ON");
                         } else if (pinValue == "off") {
                             digitalWrite(pinNumber, HIGH);  // LED 끄기
+                            Serial.print(" >> ");
                             Serial.println("LED OFF");
                         } else {
+                            Serial.print(" >> ");
                             Serial.println("Invalid value for pin control");
                             client.println("Invalid value! Use 'on' or 'off'.");
                         }
@@ -162,17 +179,21 @@ bool isSSIDAvailable(String ssid) {
     WiFi.scanDelete();      // 이전 스캔 결과 삭제
     delay(1000);            // 약간의 지연 시간 후에 스캔 시작
 
+    Serial.print(" >> ");
     Serial.println("Scanning for available networks...");
     int numNetworks = WiFi.scanNetworks();  // Wi-Fi 네트워크 스캔
 
     for (int i = 0; i < numNetworks; i++) {
+        Serial.print(" >> ");
         Serial.println("Scanning for available networks..." + ssid);
         if (WiFi.SSID(i) == ssid) {  // 입력된 SSID가 존재하는지 확인
+            Serial.print(" >> ");
             Serial.print("Found SSID: ");
             Serial.println(ssid);
             return true;
         }
     }
+    Serial.print(" >> ");
     Serial.println("SSID not found.");
     return false;
 }
@@ -183,6 +204,7 @@ void connectToWiFi(WiFiClient client, String ssid, String password) {
     WiFi.disconnect(true);  // 이전 연결 및 캐싱된 정보 모두 삭제
     delay(1000);            // 약간의 지연을 줘서 네트워크 모듈이 리셋될 시간을 확보
 
+    Serial.print(" >> ");
     Serial.println("Attempting to connect to Wi-Fi...");
     WiFi.begin(ssid.c_str(), password.c_str());  // 입력받은 SSID와 비밀번호로 연결 시도
 
@@ -195,12 +217,16 @@ void connectToWiFi(WiFiClient client, String ssid, String password) {
 
     if (WiFi.status() == WL_CONNECTED) {
         Serial.println("");
+        Serial.print(" >> ");
         Serial.println("WiFi connected.");
+
+        Serial.print(" >> ");
         Serial.println("IP address: ");
         Serial.println(WiFi.localIP());
 
         // Wi-Fi 연결에 성공한 후에 AP 모드 종료
         // WiFi.softAPdisconnect(true);
+        // Serial.print(" >> ");
         // Serial.println("AP mode stopped.");
 
         // SSID 및 비밀번호를 Preferences에 저장
@@ -212,7 +238,7 @@ void connectToWiFi(WiFiClient client, String ssid, String password) {
         // HTML 반환 : wifi connected
         client.print("<p>wifi connected.</p><br/>");
     } else {
-        Serial.println("");
+        Serial.print(" >> ");
         Serial.println("Failed to connect to Wi-Fi during 3 seconds");
 
         // HTML 반환 : wifi not connected
