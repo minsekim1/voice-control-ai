e887928f53b1b3bb90edcaf9e4609bbbc78a8581
minsekim1 <tkarnrwl7862@naver.com>
2025-03-30 13:07:11 +0900

    update endpoints

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,8 +1,6 @@
 from fastapi import APIRouter
-from app.api.endpoints import recognition, devices, settings
+from app.api.endpoints import recognition
 
 router = APIRouter()
 
 router.include_router(recognition.router, prefix="/recognition", tags=["recognition"])
-router.include_router(devices.router, prefix="/devices", tags=["devices"])
-router.include_router(settings.router, prefix="/settings", tags=["settings"]) 
